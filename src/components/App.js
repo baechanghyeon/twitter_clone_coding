6854707e5419559255e99056f562ee7164cc40f0
baechanghyeon <pek5302@naver.com>
2022-12-27 20:45:39 +0900

    getAuth 설정

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, { useState } from "react";
+import AppRouter from "components/Router";
+import { auth } from "fbinstance";
 
 function App() {
-  return <AppRouter />;
+  console.log(auth.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
 }
 
 export default App;
