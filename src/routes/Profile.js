43a18b883493ae136edb5bc683d5c76b8dd08e8f
baechanghyeon <pek5302@naver.com>
2022-12-28 03:54:53 +0900

    Profile Page LogOut Button 설정

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,20 @@
+import { getAuth } from "firebase/auth";
 import React from "react";
+import { useHistory } from "react-router-dom";
+const Profile = () => {
+  const auth = getAuth();
+  const history = useHistory();
+  // LogOut Event Click
+  const onLogOutClick = () => {
+    auth.signOut();
+    history.push("/");
+  };
 
-const Profile = () => <span>Profile</span>;
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
