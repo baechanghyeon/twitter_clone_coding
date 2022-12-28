af0e9a78c3b4767ee0298233cecd37365b6956eb
baechanghyeon <pek5302@naver.com>
2022-12-29 01:47:32 +0900

    useRef Hook 사용

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,12 +8,14 @@ import {
   orderBy,
 } from "firebase/firestore";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
   const [attachment, setAttachment] = useState();
+
+  const fileInput = useRef();
 
   useEffect(() => {
     const q = query(
@@ -66,7 +68,10 @@ const Home = ({ userObj }) => {
     reader.readAsDataURL(theFile);
   };
 
-  const onClearPhotoClick = () => setAttachment(null);
+  const onClearPhotoClick = () => {
+    setAttachment(null);
+    fileInput.current.value = null;
+  };
 
   return (
     <div>
@@ -78,11 +83,21 @@ const Home = ({ userObj }) => {
           placeholder="what's on yout mind?"
           maxLength={120}
         />
-        <input type="file" accept="image/*" onChange={onFileChange} />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={onFileChange}
+          ref={fileInput}
+        />
         <input type="submit" value="Nweet" />
         {attachment && (
           <div>
-            <img src={attachment} alt="select Img" width="50px" height="50px" />
+            <img
+              src={attachment}
+              alt="PreView Img"
+              width="50px"
+              height="50px"
+            />
             <button onClick={onClearPhotoClick}>Clear</button>
           </div>
         )}
