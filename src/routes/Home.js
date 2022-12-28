219309b5ab9030f5682ac8be954a5bdf10019856
baechanghyeon <pek5302@naver.com>
2022-12-28 16:52:39 +0900

    Home Form 완성 & firebase Database 설정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={nweet}
+          onChange={onChange}
+          placeholder="what's on yout mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
